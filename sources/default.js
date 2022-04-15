0227f3329813a4d6e8d64141df7c1150de17c12b
nathanTi <102358700+ntillier@users.noreply.github.com>
2022-04-15 16:29:47 +0200

    Update default.js

--- sources/default.js
+++ sources/default.js
@@ -1,7 +1,7 @@
 (function () {
   setTimeout(() => {
     var header =
-      '<label>nathanTi</label><div><a href="./home">HOME</a><a href="./tools">TOOLS</a><a href="./programs">PROGRAMS</a><a href="./tutorials">TUTORIALS</a><a href="./about">ABOUT</a></div>';
+      '<label>nathanTi</label><div><a href="./">HOME</a><a href="./tools">TOOLS</a><a href="./programs">PROGRAMS</a><a href="./tutorials">TUTORIALS</a><a href="./about">ABOUT</a></div>';
     var footer =
       '<div><a href="https://www.khanacademy.org/profile/kaid_1091820158849429237197523">Khan Academy</a><a href="https://github.com/nathanTi-Programing">GitHub</a><img src="https://github.com/nathanTi-Programing/nathantiprograming-webpage/blob/master/logo.png?raw=true"><span>Copyright © 2022 - Nathan Tillier</span></div>';
     document.getElementsByTagName('header')[0].innerHTML = header;
