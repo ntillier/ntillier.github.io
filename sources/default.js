09e5256ec578296a14207bcbbe3651242db287db
nathanTi <102358700+ntillier@users.noreply.github.com>
2022-04-15 17:23:39 +0200

    Update default.js

--- sources/default.js
+++ sources/default.js
@@ -3,7 +3,7 @@
     var header =
       '<label>nathanTi</label><div><a href="./">HOME</a><a href="./tools">TOOLS</a><a href="./programs">PROGRAMS</a><a href="./about">ABOUT</a></div>';
     var footer =
-      '<div><a href="https://www.khanacademy.org/profile/kaid_1091820158849429237197523">Khan Academy</a><a href="https://github.com/nathanTi-Programing">GitHub</a><img src="https://github.com/nathanTi-Programing/nathantiprograming-webpage/blob/master/logo.png?raw=true"><span>Copyright © 2022 - Nathan Tillier</span></div>';
+      '<div><a href="https://www.khanacademy.org/profile/kaid_1091820158849429237197523">Khan Academy</a><a href="https://github.com/nathanTi-Programing">GitHub</a><img src="https://raw.githubusercontent.com/ntillier/nathantiprograming-webpage/annex/logo.png"><span>Copyright © 2022 - Nathan Tillier</span></div>';
     document.getElementsByTagName('header')[0].innerHTML = header;
     document.getElementsByTagName('footer')[0].innerHTML = footer;
   }, 50);
